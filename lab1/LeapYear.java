16ce3fdb82400d02a8f1be4b67155e867a15e437
wanghy <wanghengyi@gmail.com>
2021-04-25 11:45:11 +0800

    Finish lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,8 +1,15 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Hengyi Wang
  */
 public class LeapYear {
-
+    
+    public static boolean isLeapYear(int year){
+        if(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+            return true;
+        else
+            return false;
+    }
+    
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
@@ -13,7 +20,7 @@ public class LeapYear {
             System.out.printf("%d is not a leap year.\n", year);
         }
     }
-
+    
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
